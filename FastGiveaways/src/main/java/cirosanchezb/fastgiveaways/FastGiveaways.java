571205223195f5931151d294341ddc0ad4a508d6
CiroSanchezB <120525376+CiroSanchezB@users.noreply.github.com>
2023-04-03 11:26:33 -0500

    Initial Commit 1

---
+++ FastGiveaways/src/main/java/cirosanchezb/fastgiveaways/FastGiveaways.java
@@ -0,0 +1,17 @@
+package cirosanchezb.fastgiveaways;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class FastGiveaways extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
